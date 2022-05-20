ca8835d07d9a0df651ceb028c44519c78e6ce0c6
Maarten Narain <mnarain@qualogy.com>
2022-05-20 19:54:34 -0300

    oplossing

--- src/sr/unasat/methods/app/Application.java
+++ src/sr/unasat/methods/app/Application.java
@@ -7,8 +7,8 @@ public class Application {
     public static void main(String[] args) {
 
         TafelService ts = new TafelService();
-       // ts.printMethods();
-       // ts.printMethods("Er is 1 method in de tafelservice class");
+        ts.printMethods();
+        ts.printMethods("Er is 1 method in de tafelservice class");
         ts.printMethods("1 hoedje", "2 hoedjes");
         ts.printMethods("1 hoedje", "2 hoedjes", "3 hoedjes");
         ts.printMethods("1 hoedje", "2 hoedjes", "3 hoedjes", "4 hoedjes");

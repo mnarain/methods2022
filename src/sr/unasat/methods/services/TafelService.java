ac22997fae5c649d06671c7372c4b8d4338b6a07
Maarten Narain <mnarain@qualogy.com>
2022-05-21 09:21:34 -0300

    oplossing

--- src/sr/unasat/methods/services/TafelService.java
+++ src/sr/unasat/methods/services/TafelService.java
@@ -2,44 +2,40 @@ package sr.unasat.methods.services;
 
 public class TafelService {
 
-    public int printMethods(){
+    public void printMethods(){
         System.out.println("No methods in the tafel service class");
-        return 0;
     }
 
-    public int printMethods(String m1){
+    public void printMethods(String m1){
         System.out.println(m1);
-        return 1;
     }
 
-    public int printMethods(String m1, String m2){
+    public void printMethods(String m1, String m2){
         System.out.println(m1);
         System.out.println(m2);
-        return 2;
     }
 
-    public int printMethods(String m1, String m2, String m3){
+
+
+    public void printMethods(String m1, String m2, String m3){
         System.out.println(m1);
         System.out.println(m2);
         System.out.println(m3);
-        return 3;
     }
 
-    public int printMethods(String m1, String m2, String m3, String m4){
+    public void printMethods(String m1, String m2, String m3, String m4){
         System.out.println(m1);
         System.out.println(m2);
         System.out.println(m3);
         System.out.println(m4);
-        return 4;
     }
 
-    public int printMethods(String m1, String m2, String m3, String m4, String m5){
+    public void printMethods(String m1, String m2, String m3, String m4, String m5){
         System.out.println(m1);
         System.out.println(m2);
         System.out.println(m3);
         System.out.println(m4);
         System.out.println(m5);
-        return 5;
     }
 
 }

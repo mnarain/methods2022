f5c65d718bc36e2557a242d9807bcb9b20f1bafb
Maarten Narain <mnarain@qualogy.com>
2022-05-21 12:32:51 -0300

    oplossing

---
+++ src/sr/unasat/methods/w3schools/SwitchExample.java
@@ -0,0 +1,8 @@
+package sr.unasat.methods.w3schools;
+
+public class SwitchExample {
+}
+
+
+//ik heb een vending machine met 5 verschillende dranken
+//gebruik een switch om een drank te kunnen kiezen en de prijs door te geven.

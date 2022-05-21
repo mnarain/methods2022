8358d4ce0e7bed6ecd9b12efb14fa0858e49a748
Maarten Narain <mnarain@qualogy.com>
2022-05-21 12:29:30 -0300

    oplossing

--- src/sr/unasat/methods/w3schools/IfExamples.java
+++ src/sr/unasat/methods/w3schools/IfExamples.java
@@ -3,16 +3,22 @@ package sr.unasat.methods.w3schools;
 public class IfExamples {
 
     public static void main(String[] args) {
-   int pincode = 1234;
-    int ingvoerdePincode = 1234;
-    double pinBedrag = 1000;
+        int pincode = 1234;
+        int ingvoerdePincode = 1234;
+        double pinBedrag = 1000;
 
-    if(){
-        //indien de pincode correct is controlleer of het gevraagde bedrag maximaal 1000srd is
-        //indien het bedrag groter dan 1000srd is geef aan dat dit bedrag niet is toegestaan
-    }else{
-        //geef hier aan aat de pincode fout is
-    }
+        if (pincode == ingvoerdePincode) {
+            //indien de pincode correct is controlleer of het gevraagde bedrag maximaal 1000srd is
+            if (pinBedrag <= 1000) {
+                System.out.println("Het aangevraagde bedrag van: " + pinBedrag + " wordt verstrekt");
+            } else {
+                //indien het bedrag groter dan 1000srd is geef aan dat dit bedrag niet is toegestaan
+                System.out.println("Het aangevraagd bedrag van: " + pinBedrag + " is meer dan het toegestaan bedrag van 1000");
+            }
+        } else {
+            //geef hier aan aat de pincode fout is
+            System.out.println("Uw pincode is incorrect");
+        }
 
 
     }
